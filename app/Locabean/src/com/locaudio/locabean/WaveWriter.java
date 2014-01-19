a6c266d5b4c85a9b7d94b43e5f018cc7be834ee1
Alexander James Wallar <aw204@st-andrews.ac.uk>
2014-01-19 02:14:30 -0500

    Changed layout

---
+++ app/Locabean/src/com/locaudio/locabean/WaveWriter.java
@@ -0,0 +1,110 @@
+package com.locaudio.locabean;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+
+public class WaveWriter {
+	@SuppressWarnings("deprecation")
+	protected static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+	protected static final int RECORDER_BPP = 16;
+	protected static final int RECORDER_SAMPLERATE = 44100;
+	protected static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+
+	protected static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(
+			RECORDER_SAMPLERATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
+
+	protected static void writeWaveFileHeader(FileOutputStream out,
+			long totalAudioLen, long totalDataLen, long longSampleRate,
+			int channels, long byteRate, int bitsPerSample) throws IOException {
+
+		byte[] header = new byte[44];
+
+		header[0] = 'R'; // RIFF/WAVE header
+		header[1] = 'I';
+		header[2] = 'F';
+		header[3] = 'F';
+		header[4] = (byte) (totalDataLen & 0xff);
+		header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+		header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+		header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+		header[8] = 'W';
+		header[9] = 'A';
+		header[10] = 'V';
+		header[11] = 'E';
+		header[12] = 'f'; // 'fmt ' chunk
+		header[13] = 'm';
+		header[14] = 't';
+		header[15] = ' ';
+		header[16] = 16; // 4 bytes: size of 'fmt ' chunk
+		header[17] = 0;
+		header[18] = 0;
+		header[19] = 0;
+		header[20] = 1; // format = 1
+		header[21] = 0;
+		header[22] = (byte) channels;
+		header[23] = 0;
+		header[24] = (byte) (longSampleRate & 0xff);
+		header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+		header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+		header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+		header[28] = (byte) (byteRate & 0xff);
+		header[29] = (byte) ((byteRate >> 8) & 0xff);
+		header[30] = (byte) ((byteRate >> 16) & 0xff);
+		header[31] = (byte) ((byteRate >> 24) & 0xff);
+		header[32] = (byte) (2 * 16 / 8); // block align
+		header[33] = 0;
+		header[34] = (byte) bitsPerSample; // bits per sample
+		header[35] = 0;
+		header[36] = 'd';
+		header[37] = 'a';
+		header[38] = 't';
+		header[39] = 'a';
+		header[40] = (byte) (totalAudioLen & 0xff);
+		header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+		header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+		header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+
+		out.write(header, 0, 44);
+	}
+
+	protected static void copyWaveFile(String inFilename, String outFilename) {
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		long totalAudioLen = 0;
+		long totalDataLen = totalAudioLen + 36;
+		long longSampleRate = RECORDER_SAMPLERATE;
+		int channels = 1;
+		long byteRate = RECORDER_BPP * RECORDER_SAMPLERATE * channels / 8;
+
+		byte[] data = new byte[BUFFER_SIZE];
+
+		try {
+			in = new FileInputStream(inFilename);
+			out = new FileOutputStream(outFilename);
+			totalAudioLen = in.getChannel().size();
+			totalDataLen = totalAudioLen + 36;
+
+			System.out.println("File size: " + totalDataLen);
+
+			writeWaveFileHeader(out, totalAudioLen, totalDataLen,
+					longSampleRate, channels, byteRate, RECORDER_BPP);
+
+			while (in.read(data) != -1) {
+				out.write(data);
+			}
+
+			in.close();
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
