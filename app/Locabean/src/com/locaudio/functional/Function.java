dd0fb673de5f61fc5854762a177a29f0f7db438f
Alexander James Wallar <aw204@st-andrews.ac.uk>
2014-01-21 17:31:05 -0500

    Added more functional capabilities

--- app/Locabean/src/com/locaudio/functional/Function.java
+++ app/Locabean/src/com/locaudio/functional/Function.java
@@ -1,9 +1,36 @@
 package com.locaudio.functional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Function<T, R> {
+
 	public abstract R body(T input);
-	
+
 	public R call(T input) {
 		return body(input);
 	}
+
+	public <IN_TYPE, OUT_TYPE> List<OUT_TYPE> map(
+			Function<IN_TYPE, OUT_TYPE> f, IN_TYPE[] inArray) {
+
+		List<OUT_TYPE> retList = new ArrayList<OUT_TYPE>();
+		for (IN_TYPE inVal : inArray) {
+			retList.add(f.call(inVal));
+		}
+
+		return retList;
+	}
+
+	public <IN_TYPE, OUT_TYPE> List<OUT_TYPE> map(
+			Function<IN_TYPE, OUT_TYPE> f, List<IN_TYPE> inArray) {
+
+		List<OUT_TYPE> retList = new ArrayList<OUT_TYPE>();
+		for (IN_TYPE inVal : inArray) {
+			retList.add(f.call(inVal));
+		}
+
+		return retList;
+	}
+
 }
